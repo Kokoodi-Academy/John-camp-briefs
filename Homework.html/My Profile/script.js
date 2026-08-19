6e14992c1e8b46a697e538cf24ecb76d09910205
JohnOlaleye <johnolaleye2026@outlook.com>
2026-08-19 09:14:22 +0000

    Commmitted my homework

---
+++ Homework.html/My Profile/script.js
@@ -0,0 +1,6 @@
+const title = document.getElementById("profile-title");
+const button = document.getElementById("click");
+
+button.addEventListener("click", function () {
+	title.textContent = "Welcome to my Website!";
+});
